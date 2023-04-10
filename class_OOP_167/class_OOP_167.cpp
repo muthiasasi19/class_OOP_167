71c05a2f5cf58d425c82ebd39b06ce981edf41aa
muthiasasi19 <muthiasasimarsakina19@gmail.com>
2023-04-10 01:02:53 -0700

    membuat access modifier

--- class_OOP_167/class_OOP_167.cpp
+++ class_OOP_167/class_OOP_167.cpp
@@ -17,4 +17,14 @@ private:
 	string kode;
 	string namaMK;
 public:
+	void inputMK() {
+
+		cout << "\nMasukkan jumlah SKS= ";
+		cin >> sks;
+		cout << "\nMasukkan Kode MK= ";
+		cin >> kode;
+		cout << "\nMasukkan Nama MK= ";
+		cin >> namaMK;
+
+	}
 };
